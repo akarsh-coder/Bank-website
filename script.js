fc6baa89ac3ac5f34374118778177cec603a5a62
akarsh-coder <kra.getit@gmail.com>
2022-08-20 00:07:22 +0530

    some style and script added(DOM)

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
@@ -61,13 +59,29 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
+
+const displayMovements=function(movements){
+  containerMovements.innerHTML="";
+movements.forEach(function(mov,i){
+
+const type=mov>0?'deposit':'withdrawal';
+
+  const html = `<div class="movements__row">
+  <div class="movements__type movements__type--${type}">
+    ${i+1} ${type}
+  </div>
+  <div class="movements__date">24/01/2037</div>
+  <div class="movements__value">${mov}₹</div>
+</div>`;
+
+  containerMovements.insertAdjacentHTML('afterbegin',html);
+});
+}
+displayMovements(account1.movements)
 
